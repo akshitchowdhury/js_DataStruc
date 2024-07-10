774e9ce677559b6f652b0ec68268fe9ea078f231
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-07-11 01:02:40 +0530

    arrays3

---
+++ ReverseArray.js
@@ -0,0 +1,16 @@
+//reverse the array
+
+let arr = [1,2,3,4,5,6]
+
+function reverse(arr){
+
+    const revArr = new Array(arr.length)
+
+    for(let i=0; i<arr.length; i++){
+        revArr[i] = arr[arr.length-1-i]
+    }
+
+    return revArr;
+}
+
+console.log(reverse(arr))
