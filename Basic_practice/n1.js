1f69af612de53b995ab9cd6c88db796755a16c3b
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-07-30 00:55:53 +0530

    revWord2

--- Basic_practice/n1.js
+++ Basic_practice/n1.js
@@ -2,7 +2,14 @@ let word = "w3resource";
 
 let wordArr = word.split("");
 
-let revWord = wordArr.reverse().join("");
+// let revWord = wordArr.reverse().join("");
 
-console.log(revWord);
+let revWord = [];
+for(let i=wordArr.length-1; i>=0; i--){
+ 
+    revWord[i-1] = wordArr[i];
+    
+}
+
+console.log(revWord.join(""))
 
