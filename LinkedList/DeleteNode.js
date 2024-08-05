e0d95ba8bf018b1c366d7e1cb8311bb7d8681aec
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-06 00:52:19 +0530

    llDeletion

---
+++ LinkedList/DeleteNode.js
@@ -0,0 +1,69 @@
+class Node{
+    constructor(newData){
+        this.data = newData;
+        this.next = null;
+    }
+}
+
+function deleteFromBeginning(head){
+
+    if(head===null){
+        return null;
+    }
+
+    head = head.next;
+    return head;
+    
+}
+
+function deleteFromLast(head){
+    if (head === null) {
+        return null;
+    }
+
+    if (head.next === null) {
+        return null;
+    }
+
+    let curr = head;
+    
+
+    while (curr.next.next != null) {
+        curr = curr.next; //keep travelling until second last node is not reached
+    }
+
+    // Set the second last node's next pointer to null
+    curr.next = null; //this curr.next here is already the 2nd last node now
+
+    return head;
+}
+
+
+function printList(head){
+
+    let current = head;
+    let result = "";
+
+    while(current!= null){
+        result += " "+current.data;
+        current = current.next;
+    }
+
+    console.log(result);
+}
+
+function main(){
+
+    let header = new Node(12);
+    header.next = new Node(13);
+    header.next.next = new Node(14);
+    header.next.next.next = new Node(15);
+
+    printList(header);
+
+    header = deleteFromLast(header);
+
+    printList(header);
+}
+
+main();
